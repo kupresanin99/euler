50caf10e3b5726210c0e301e620a9dbf95b26d95
Joe Kupresanin <28194478+kupresanin99@users.noreply.github.com>
2021-12-11 07:02:54 -0600

    euler problem 29 correct

---
+++ 029_problem.py
@@ -0,0 +1,12 @@
+# For a in [2, 100] and b in [2, 100], obtain the distinct set of 
+# a^b and b^a, removing duplicates.  
+
+my_set = {4} # We know 4 is in the result set
+
+for a in range(2, 101):
+  temp_list = []
+  for b in range(2, 101):
+    temp_list.append(a ** b)
+    temp_list.append(b ** a)
+  my_set = my_set.union(set(temp_list))
+  print(f"a = {a} and result set has {len(my_set)} entries")
