89605e0858b2bc95f40fcb50f83cc9a789dce7a0
Joe Kupresanin <28194478+kupresanin99@users.noreply.github.com>
2021-12-11 22:17:04 -0600

    euler problem 25 successful

---
+++ 025_problem.py
@@ -0,0 +1,11 @@
+f1 = 1
+f2 = 1
+index = 2
+digits = 1
+while digits < 1000:
+  index += 1
+  current = f1 + f2
+  f1 = f2
+  f2 = current
+  digits = len(str(f2))
+print(f"index = {index} has digits = {digits}")
--- 048_problem.py
+++ 048_problem.py
@@ -5,3 +5,9 @@ for i in range(1, 1001):
 
 my_list = list(str(sum))
 print(my_list[-10:])
+
+sum = 0
+for j in range(1, 1001):
+  sum += j ** j % 10 ** 10
+my_list = list(str(sum))
+print(my_list[-10:])
