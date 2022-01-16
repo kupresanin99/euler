3a8db83ff551d55f238f97d51bb93b14b01a1a88
Joe Kupresanin <28194478+kupresanin99@users.noreply.github.com>
2022-01-15 20:02:24 -0600

    I abhor Euler 18

--- 018_problem.py
+++ 018_problem.py
@@ -1,22 +1,36 @@
+
 file = open('018_problem.txt', 'r')
 list_of_lists = []
 line = file.readline()
+longest_line = 0
 while line:
+  longest_line += 1
   list_of_lists.append([int(x) for x in line.strip().split()])
   line = file.readline()
 file.close()
-list_of_bingos = []
-rows = len(list_of_lists)
-while rows >= 1:
-  left_sum = sum([left[0] for left in list_of_lists])
-  right_sum = sum([right[-1] for right in list_of_lists])
-  list_of_bingos.append(list_of_lists[0][0])
-  print(list_of_bingos)
-  print(sum(list_of_bingos))
+
+for my_item in list_of_lists:
+  for j in range(longest_line - len(my_item)):
+    my_item.append(0)
+list_of_lists = list_of_lists[::-1]
+my_sum = [0] * len(list_of_lists)
+
+for i in range(1, len(list_of_lists)):
+  if i == 1:
+    bottom = list_of_lists[0]
+    top = list_of_lists[1]
+  else:
+    bottom = my_sum
+    top = list_of_lists[i]
+  j = 0
+  print(f"top = {top}")
+  print(f"bottom = {bottom}")
+  for num in top:
+    print(f"num = {num}")
+    if num != 0:
+      print(f"max = {max(num + bottom[j], num + bottom[j + 1])}")
+      my_sum[j] = max(num + bottom[j], num + bottom[j + 1])
+      j += 1
+      print(my_sum)
   
-  if left_sum > right_sum:
-    list_of_lists = [x[:-1] for x in list_of_lists]
-  else: 
-    list_of_lists = [x[1:] for x in list_of_lists]
-  list_of_lists = list_of_lists[1:]
-  rows = len(list_of_lists)
+  
