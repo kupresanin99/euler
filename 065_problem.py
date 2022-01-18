e709323c2f4e4f19ea4a252264a465b2fe0d7ae2
Joe Kupresanin <28194478+kupresanin99@users.noreply.github.com>
2022-01-17 19:16:14 -0600

    Euler 65 cleaned up a bit

--- 065_problem.py
+++ 065_problem.py
@@ -6,12 +6,10 @@ for j in range(1, 34):
   list_of_e.append(1)
   i += 1
 list_of_e = list_of_e[::-1]
-print(list_of_e)
+
 D = list_of_e[0]
 N = 1
-print(D)
 list_of_e = list_of_e[1:]
-print(list_of_e)
 
 for digit in list_of_e:
   N = D * digit + N
@@ -19,12 +17,10 @@ for digit in list_of_e:
   D = N
   N = temp
 
-  print(f"Numerator = {N:,}")
-  print(f"Denominator = {D:,}")
-  print(f"{D/N:,.60f}")
-
 my_string = str(D)
 my_sum = 0
 for dig in my_string:
   my_sum += int(dig)
-print(my_sum)
+print(f"Numerator = {D:,}")
+print(f"Denominator = {N:,}")
+print(f"Sum of numerator digits is: {my_sum}")
