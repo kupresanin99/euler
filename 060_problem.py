f33f4e64afdae29182dfd163572d4ea79c4493b0
Joe Kupresanin <28194478+kupresanin99@users.noreply.github.com>
2022-01-29 08:33:04 -0600

    Have all 3 prime concats saved

--- 060_problem.py
+++ 060_problem.py
@@ -7,15 +7,20 @@ for i in range (2, 100000):
   if prime(i):
     list_of_primes.append(i)
 
-list_of_primes_to_99 = [x for x in list_of_primes if x <= 9999 and x != 2 and x != 5]
+list_of_primes_to_99 = [x for x in list_of_primes if x <= 999 and x != 2 and x != 5]
 max_concats = 0
-for thing in combinations(list_of_primes_to_99, 5):
+list_of_3_tupes = []
+for thing in combinations(list_of_primes_to_99, 3):
   prime_counter = 0
   for pair in permutations(thing, 2):
     if not prime(int(str(pair[0]) + str(pair[1]))):
       break
     else:
       prime_counter += 1
+  if prime_counter == 6:
     print(f"""{thing} has {prime_counter} prime concats""")
-  if max_concats == 5:
-    break
+    single_3_tupe = []
+    for num in thing:
+      single_3_tupe.append(num)
+    list_of_3_tupes.append(single_3_tupe)
+print(list_of_3_tupes)
